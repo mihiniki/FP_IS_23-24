93ec95468e973a413fd3a3190d75f69d48e3e142
mihiniki <amnz@abv.bg>
2024-04-20 18:48:52 +0300

    Add tasks.

---
+++ Week_08 - Algebraic Types. Nonlinear Data Structures/task4.hs
@@ -0,0 +1,20 @@
+{-
+Create a new data type called `Point2D` and define a function
+that accepts two functions: `f` and `g` and a list of points
+with two coordinates. The function should return
+those points for which `f(firstCoordinate) = g(secondCoordinate)`
+and should be able to work with whole
+as well as floating point numbers!
+
+**Acceptance criteria:**
+
+1. All tests pass.
+2. `Point2D` is a new data type in Haskell.
+3. Create `myImagesFold` using folding.
+-}
+
+main :: IO()
+main = do
+    print $ myImages (\x -> x * x) (2+) [Point 2 2, Point 1 2, Point 3 7] == [Point 2 2, Point 3 7]
+
+
---
+++ Week_08 - Algebraic Types. Nonlinear Data Structures/task5.hs
@@ -0,0 +1,26 @@
+{-
+By using the `Shape` data type, define the following functions:
+
+1. isRound - returns whether a shape has a round side;
+2. is2D - returns whether a shape has two dimensions.
+
+**Acceptance criteria:**
+
+1. All tests pass.
+-}
+
+main :: IO()
+main = do
+    print $ isRound (Circle 5) == True
+    print $ isRound (Rectangle 2.5 4.5) == False
+    print $ isRound (Rectangle 5.5 20.6) == False
+    print $ isRound (Triangle 5.3 3.9 4.89) == False
+    print $ isRound (Cylinder 20 30) == True
+
+    print $ is2D (Circle 5) == True
+    print $ is2D (Rectangle 2.5 4.5) == True
+    print $ is2D (Rectangle 5.5 20.6) == True
+    print $ is2D (Triangle 5.3 3.9 4.89) == True
+    print $ is2D (Cylinder 20 30) == False
+
+
